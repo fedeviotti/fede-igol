1b509021d1760cd7eaa882db119f4eb5abee0e53
Federico Viotti <fedeviotti@gmail.com>
2025-07-27 23:54:30 +0200

    fix: update TODO comment for vehicle insertion

--- src/app/(root-layout)/maintenance/page.tsx
+++ src/app/(root-layout)/maintenance/page.tsx
@@ -1,7 +1,7 @@
 import { VehiclesPage } from '@/app/(root-layout)/maintenance/components/VehiclesPage';
 
 export default function MaintenancePage() {
-  // TODO: fare form inserimento veicolo e verificare che la tabella
+  // TODO: fare form inserimento veicolo (insertVehicle) e verificare che la tabella
   //  venga aggioranta (grazie a revalidatePath)
   return <VehiclesPage />;
 }
