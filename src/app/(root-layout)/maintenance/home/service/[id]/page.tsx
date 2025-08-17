ef54f9d03ae1dbe2390d38d8b36a30437c163c6a
Federico Viotti <fedeviotti@gmail.com>
2025-08-17 19:03:59 +0200

    chore: add debug log in service page

--- src/app/(root-layout)/maintenance/home/service/[id]/page.tsx
+++ src/app/(root-layout)/maintenance/home/service/[id]/page.tsx
@@ -32,6 +32,8 @@ export default function ServiceDetailPage({ params }: Props) {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
+  console.log('test');
+
   const renderVehicleTypeIcon = (type: string) => {
     switch (type) {
       case 'car':
